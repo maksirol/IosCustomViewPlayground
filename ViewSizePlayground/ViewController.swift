4f1fedc577cc5b486bc05fb2c33f26b5579172b8
Roland Maksimovicz <roland.maksimovicz@visma.com>
2020-02-28 18:28:54 +0200

    Initial Commit

---
+++ ViewSizePlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewSizePlayground
+//
+//  Created by Roland Maksimovicz on 2020-02-28.
+//  Copyright © 2020 macrol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
